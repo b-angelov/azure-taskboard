42ee60e9329b1c8ea7b6103c22ff48bde86746f5
b-angelov <sauronthegreat76@gmail.com>
2025-07-29 17:56:02 +0300

    repaired

--- TaskBoard.WebApp/Program.cs
+++ TaskBoard.WebApp/Program.cs
@@ -10,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseMySql(connectionString)); ;
+    options.UseSqlServer(connectionString)); ;
 
 builder.Services
     .AddDefaultIdentity<User>(options =>
